16ce8a5080d9ad30de45a668a2b0ae7769999b1e
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2023-01-25 22:35:42 -0500

    ...

--- gemm/MainMulticluster.jl
+++ gemm/MainMulticluster.jl
@@ -2,8 +2,8 @@ using Hash
 using MulticlusterManager
 
 #@cluster local_cluster "heron@localhost" 6 dir=`/home/heron/Dropbox/Copy/ufc_mdcc_hpc/Hash.jl/gemm` exename=`/opt/julia-1.8.3/bin/julia` tunnel=true
-@cluster cluster_1 "tcarneiropessoa@grvingt-1.nancy.grid5000.fr"  6 hostfile="/home/tcarneiropessoa/hostfile.1" dir=`/home/tcarneiropessoa/heron/Hash.jl/gemm` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
-@cluster cluster_2 "tcarneiropessoa@graoully-1.nancy.grid5000.fr" 6 hostfile="/home/tcarneiropessoa/hostfile.2" dir=`/home/tcarneiropessoa/heron/Hash.jl/gemm` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
+@cluster cluster_1 "tcarneiropessoa@grvingt-1.nancy.grid5000.fr"  6 hostfile=`/home/tcarneiropessoa/hostfile.1` dir=`/home/tcarneiropessoa/heron/Hash.jl/gemm` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
+@cluster cluster_2 "tcarneiropessoa@graoully-1.nancy.grid5000.fr" 6 hostfile=`/home/tcarneiropessoa/hostfile.2` dir=`/home/tcarneiropessoa/heron/Hash.jl/gemm` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
 #@cluster cluster_3 "tcarneiropessoa@grvingt-1.nancy.grid5000.fr" 6 dir=`/home/tcarneiropessoa/heron/Hash.jl/gemm` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
 #@cluster cluster_4 "tcarneiropessoa@grvingt-1.nancy.grid5000.fr" 6 dir=`/home/tcarneiropessoa/heron/Hash.jl/gemm` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
 
