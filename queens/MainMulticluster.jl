d3ca9ee9e43f35a7f14ad0416e913e461098b69a
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2023-01-27 14:18:50 -0500

    ...

--- queens/MainMulticluster.jl
+++ queens/MainMulticluster.jl
@@ -2,8 +2,8 @@ using Hash
 using MulticlusterManager
 
 #@cluster local_cluster "heron@localhost" 6 dir=`/home/heron/Dropbox/Copy/ufc_mdcc_hpc/Hash.jl/queens` exename=`/opt/julia-1.8.3/bin/julia` tunnel=true
-@cluster cluster_1 "tcarneiropessoa@grvingt-10.nancy.grid5000.fr"  6 hostfile=`/home/tcarneiropessoa/hostfile.1` dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
-@cluster cluster_2 "tcarneiropessoa@graoully-12.nancy.grid5000.fr" 6 hostfile=`/home/tcarneiropessoa/hostfile.2` dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
+@cluster cluster_1 "tcarneiropessoa@grvingt-13.nancy.grid5000.fr"  6 hostfile=`/home/tcarneiropessoa/hostfile.1` dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
+@cluster cluster_2 "tcarneiropessoa@graoully-11.nancy.grid5000.fr" 6 hostfile=`/home/tcarneiropessoa/hostfile.2` dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
 @cluster cluster_3 "tcarneiropessoa@grcinq-13.nancy.grid5000.fr" 6 hostfile=`/home/tcarneiropessoa/hostfile.4` dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
 @cluster cluster_4 "tcarneiropessoa@grappe-11.nancy.grid5000.fr" 6 hostfile=`/home/tcarneiropessoa/hostfile.5` dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
 #@cluster cluster_3 "tcarneiropessoa@grvingt-1.nancy.grid5000.fr" 6 dir=`/home/tcarneiropessoa/heron/Hash.jl/queens` exename=`/home/tcarneiropessoa/julia-1.8.2/bin/julia` tunnel=true
@@ -11,6 +11,7 @@ using MulticlusterManager
 
 @launch QueensMulticluster worker:cluster_1 worker:cluster_2 worker:cluster_3 worker:cluster_4
 
+@info "START !!!"
 @time QueensMulticluster.queens(15)
 
 QueensMulticluster.finish()
